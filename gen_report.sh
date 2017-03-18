0934584b26807956135a508c7c4719ec223fa946
Brennon York <brennon.york@capitalone.com>
2017-03-18 13:40:18 -0700

    updated report gen script to output to report.txt

--- gen_report.sh
+++ gen_report.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-for p in {1..3}; do
-    echo "Problem $p"
-
+for p in {1..1}; do
     for i in {1..9}; do
-	python run_search.py -p "$p" -s "$i"
+	python run_search.py -p "$p" -s "$i" >> report.txt
     done
 done
